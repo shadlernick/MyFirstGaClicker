88549c07d4500fbb5d8ecc45f77d21a4e352600c
shadlernick <shadlernick99@gmail.com>
2019-02-05 18:26:29 +0200

    FirstCommit

---
+++ Assets/Scripts/ClickScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ClickScript : MonoBehaviour
+{
+    public Text ScoreText;
+    int score = 0;
+
+    public void OnClick()
+    {        
+        score++;
+        ScoreText.text = "Score: " + score;
+    }
+} 
