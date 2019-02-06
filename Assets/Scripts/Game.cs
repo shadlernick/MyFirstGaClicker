2353d2cef9a0b9640fef790190bd3ad44893eaae
shadlernick <shadlernick99@gmail.com>
2019-02-06 11:48:20 +0200

    added ShopPanel

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public Text ScoreText;
+    int score = 0;
+    public GameObject ShopPanel;
+
+    public void _ShopPanel()
+    {
+        ShopPanel.SetActive(!ShopPanel.activeSelf);
+    }
+
+    public void OnClick()
+    {        
+        score++;
+        ScoreText.text = "Score: " + score;
+    }
+} 
